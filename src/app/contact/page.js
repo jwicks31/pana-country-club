5b6f6237350876084a5ca6767463cc38f3e361ab
Jesse Wicks <jessewicks@gmail.com>
2023-04-07 15:16:30 -0500

    Initializing Project: Contact Page + Formatting

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -5,13 +5,108 @@ import { useRouter } from 'next/navigation';
 import { MenuBar } from '../components/MenuBar/MenuBar';
 import styles from './page.module.css';
 
+const stateOptions = [
+  'AL',
+  'AK',
+  'AZ',
+  'AR',
+  'CA',
+  'CO',
+  'CT',
+  'DE',
+  'FL',
+  'GA',
+  'HI',
+  'ID',
+  'IL',
+  'IN',
+  'IA',
+  'KS',
+  'KY',
+  'LA',
+  'ME',
+  'MD',
+  'MA',
+  'MI',
+  'MN',
+  'MS',
+  'MO',
+  'MT',
+  'NE',
+  'NV',
+  'NH',
+  'NJ',
+  'NM',
+  'NY',
+  'NC',
+  'ND',
+  'OH',
+  'OK',
+  'OR',
+  'PA',
+  'RI',
+  'SC',
+  'SD',
+  'TN',
+  'TX',
+  'UT',
+  'VT',
+  'VA',
+  'WA',
+  'WV',
+  'WI',
+  'WY',
+];
+
 const ContactForm = () => {
-  const [name, setName] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [address, setAddress] = useState('');
+  const [city, setCity] = useState('');
+  const [state, setState] = useState('IL');
+  const [postalCode, setPostalCode] = useState('');
   const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
+  const [phone, setPhone] = useState('');
+  const [comment, setComment] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const router = useRouter();
+
+  const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value);
+  };
+
+  const handleLastNameChange = (event) => {
+    setLastName(event.target.value);
+  };
+
+  const handleAddressChange = (event) => {
+    setAddress(event.target.value);
+  };
+
+  const handleCityChange = (event) => {
+    setCity(event.target.value);
+  };
+
+  const handleStateChange = (event) => {
+    setState(event.target.value);
+  };
+
+  const handlePostalCodeChange = (event) => {
+    setPostalCode(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePhoneChange = (event) => {
+    setPhone(event.target.value);
+  };
+
+  const handleCommentChange = (event) => {
+    setComment(event.target.value);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -39,36 +134,118 @@ const ContactForm = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          id="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="message">Message</label>
-        <textarea
-          id="message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        />
-      </div>
-      <button type="submit" disabled={isLoading}>
-        {isLoading ? 'Submitting...' : 'Submit'}
-      </button>
-      {error && <p>{error}</p>}
+      <fieldset>
+        <legend>Contact Information</legend>
+        <div>
+          <label htmlFor="firstName">First Name:</label>
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            value={firstName}
+            onChange={handleFirstNameChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="lastName">Last Name:</label>
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            value={lastName}
+            onChange={handleLastNameChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="address">Address:</label>
+          <input
+            type="text"
+            id="address"
+            name="address"
+            value={address}
+            onChange={handleAddressChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="city">City:</label>
+          <input
+            type="text"
+            id="city"
+            name="city"
+            value={city}
+            onChange={handleCityChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="state">State:</label>
+          <select
+            id="state"
+            name="state"
+            value={state}
+            onChange={handleStateChange}
+            required
+          >
+            {stateOptions.map((stateCode) => (
+              <option key={stateCode} value={stateCode}>
+                {stateCode}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label htmlFor="postalCode">Postal Code:</label>
+          <input
+            type="text"
+            id="postalCode"
+            name="postalCode"
+            value={postalCode}
+            onChange={handlePostalCodeChange}
+            required
+          />
+        </div>
+      </fieldset>
+      <fieldset>
+        <legend>How Should We Contact You</legend>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={handleEmailChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="phone">Phone:</label>
+          <input
+            type="tel"
+            id="phone"
+            name="phone"
+            value={phone}
+            onChange={handlePhoneChange}
+            required
+          />
+        </div>
+      </fieldset>
+      <fieldset>
+        <legend>Additional Information</legend>
+        <div>
+          <label htmlFor="comment">Comment:</label>
+          <textarea
+            id="comment"
+            name="comment"
+            value={comment}
+            onChange={handleCommentChange}
+            required
+          />
+        </div>
+      </fieldset>
     </form>
   );
 };
