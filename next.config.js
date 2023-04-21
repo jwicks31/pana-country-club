d778c64611dd87e1b20bf9d341d05f5b99610ef5
Jesse Wicks <jessewicks@gmail.com>
2023-04-21 13:17:23 -0500

    feat(redirects): redirects for old html pages

--- next.config.js
+++ next.config.js
@@ -6,6 +6,35 @@ const nextConfig = {
   images: {
     domains: ['openweathermap.org'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/contact-us.html',
+        destination: '/contact',
+        permanent: true,
+      },
+      {
+        source: '/membership-information.html',
+        destination: '/membership',
+        permanent: true,
+      },
+      {
+        source: '/golf--leauges.html',
+        destination: '/',
+        permanent: false,
+      },
+      {
+        source: '/member.html',
+        destination: '/membership',
+        permanent: true,
+      },
+      {
+        source: '/about.html',
+        destination: '/about',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig
